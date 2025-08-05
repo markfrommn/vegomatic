17b0b539af2a4ef19156a751a96c6c893dc2c255
Mark Gooderum <3393412+markfrommn@users.noreply.github.com>
2025-08-04 22:26:19 -0500

    Start DataFetch class and clean up modules

--- src/vegomatic/datafetch/datafetch.py
+++ src/vegomatic/datafetch/datafetch.py
@@ -1,54 +1,76 @@
-#
-# datafetch - A set of utilities for fetching data from a database.
-#
+"""
+datafetch - A set of utilities for fetching data from a database.
+
+This module provides a simple interface for database operations using the pydal library.
+"""
 
 import sys
-import mysql.connector
+from pydal import DAL
 from typing import Callable, Union, Tuple
 
 class DataFetch:
+    """
+    A class to manage database connections and operations using pydal.
+    
+    Attributes
+    ----------
+    db : Union[DAL, None]
+        The database connection object using pydal's DAL, or None if not connected
+    
+    Methods
+    -------
+    clear()
+        Closes the database connection and frees resources
+    create(dburl: str)
+        Creates a new database connection using the provided URL
+    """
+    
+    db: Union[DAL, None] = None
+
     def __init__(self):
+        """
+        Initialize a new DataFetch instance.
+        
+        The database connection is initially set to None and cleared.
+        """
+        self.db = None
         self.clear()
 
     def clear(self):
+        """
+        Clear and close the current database connection.
+        
+        If a database connection exists, it will be closed and resources will be freed.
+        The database connection object will be set to None.
+        
+        Returns
+        -------
+        None
+        """
+        if self.db is not None:
+            del self.db  # No real destructor/close but does free a bunch O ram
+            self.db = None
         return
 
-    def db_init(self, database, user, password, host="127.0.0.1", port=3306, dbtype="mysql"):
-        self.db_db = database
-        self.db_user = user
-        self.db_pw = password
-        self.db_host = host
-        self.db_port = port
-        self.db_type = dbtype
-        self.db_set = True
-
-    def db_connect(self, autocommit=True):
-        if not self.db_set:
-            raise ConnectionError("Connect without DB parameters set")
-        if self.db_type == "mysql":
-            self.db_conn = mysql.connector.connect(host=self.db_host,
-                                                   user=self.db_user,
-                                                   passwd=self.db_pw,
-                                                   database=self.db_db,
-                                                   port=self.db_port,
-                                                   autocommit=autocommit)
-
-    def dictlist_query(self, query: str) -> list:
+    def create(self, dburl: str) -> bool:
         """
-        :query SQL Query String:
-        :param filepath:
-        :return:
+        Create a new database connection.
+        
+        Parameters
+        ----------
+        dburl : str
+            The database URL string in pydal format
+            
+        Returns
+        -------
+        bool
+            True if the connection was successfully created
+            
+        Examples
+        --------
+        >>> df = DataFetch()
+        >>> df.create("sqlite://storage.db")
+        True
         """
-        retlist = []
-        db_cur = self.db_conn.cursor(dictionary=True)
-        try:
-            db_cur.execute(query)
-            db_result = db_cur.fetchall()
-        except mysql.connector.ProgrammingError as pe:
-            print("ERROR: query [{}]".format(query))
-            raise pe
-        for row in db_result:
-            retlist.append(row)
-        return retlist
-
-
+        self.db = DAL(dburl)
+        return True
