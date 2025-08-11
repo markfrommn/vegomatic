0db25635393079131899fce0962e7fe399b42186
Mark Gooderum <3393412+markfrommn@users.noreply.github.com>
2025-08-10 23:42:55 -0500

    - Clean up import order - Bump version number to 0.2.0

--- examples/github_fetch.py
+++ examples/github_fetch.py
@@ -3,9 +3,27 @@
 Simple example demonstrating the GqlFetch module Github module.
 """
 
-from vegomatic.gqlf_github import GqlFetchGithub
 from argparse import ArgumentParser
+import csv
+from enum import Enum
+import json
 import pprint
+import sys
+
+from vegomatic.gqlf_github import GqlFetchGithub
+
+class OutputFormat(Enum):
+    LONG = 'long'
+    SHORT = 'short'
+    JSON = 'json'
+    CSV = 'csv'
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.value
+
 
 def pretty_print(clas, indent=0):
     print(' ' * indent +  type(clas).__name__ +  ':')
@@ -19,32 +37,35 @@ def pretty_print(clas, indent=0):
 def progress_cb(current, total):
     print(f"Progress: {current} of {total}...", end='\n')
 
-def print_repo(repository, long_format=False):
-    if not long_format:
+def print_repo(repository, format: OutputFormat = OutputFormat.SHORT, outfile=sys.stdout):
+    if format == OutputFormat.SHORT:
         print("{} - Created: {}, Updated: {}, Description: {}".format(
-            repository['name'], repository['createdAt'], repository['updatedAt'], repository['description']))
+            repository['name'], repository['createdAt'], repository['updatedAt'], repository['description']), file=outfile)
     else:
-        print("Repository: {}".format(repository['name']))
+        print("Repository: {}".format(repository['name']), file=outfile)
         print_repo = repository.copy()
         del print_repo['name']
-        pprint.pprint(print_repo)
+        pprint.pprint(print_repo, stream=outfile)
+    outfile.flush()
 
-def print_pr(pr, long_format=False):
-    if not long_format:
-        print("{} - Created: {}, Merged: {}, Closed: {}".format(
-            pr['title'], pr['createdAt'], pr['mergedAt'], pr['closedAt']))
+def print_pr(pr, format: OutputFormat = OutputFormat.SHORT, outfile=sys.stdout):
+    if format == OutputFormat.SHORT:
+        print("{} - State: {}, Created: {}, Merged: {}, Closed: {}, Title: {}".format(
+            pr['number'], pr['state'], pr['createdAt'], pr['mergedAt'], pr['closedAt'], pr['title']), file=outfile)
     else:
-        print("PR: {}".format(pr['title']))
-        pprint.pprint(pr)
+        print("PR: {}".format(pr['title']), file=outfile)
+        pprint.pprint(pr, stream=outfile)
+    outfile.flush()
 
 if __name__ == "__main__":
     fetch_type = None
 
     parser = ArgumentParser(description='Github Fetch')
     parser.add_argument('--organization', type=str, required=True, help='Organization to fetch')
-    parser.add_argument('--long', action='store_true', help='Long format')
+    parser.add_argument('--format', type=OutputFormat, default=OutputFormat.SHORT, choices=list(OutputFormat))
     parser.add_argument('--print-query', action='store_true', help='Print the query')
     parser.add_argument('--repository', type=str, help='Repository to fetch')
+    parser.add_argument('--outbase', type=str, help='Output file base name')
     args = parser.parse_args()
 
     if args.repository:
@@ -66,18 +87,37 @@ if __name__ == "__main__":
         print("Query:\n{}".format(query))
         exit(0)
 
+    if args.outbase:
+        outname = args.outbase + '.' + args.format.value
+        outfile = open(outname, 'w')
+    else:
+        outfile = sys.stdout
+
     gh.connect()
 
     if fetch_type == 'repos':
         repositories = gh.get_repositories(organization=args.organization, progress_cb=progress_cb)
-        for repository in repositories:
-            print_repo(repository=repository, long_format=args.long)
+        if args.format == OutputFormat.SHORT or args.format == OutputFormat.LONG:
+            for repository in repositories:
+                print_repo(repository=repository, format=args.format, outfile=outfile)
+        elif args.format == OutputFormat.JSON:
+            json.dump(repositories, outfile)
+        elif args.format == OutputFormat.CSV:
+            csv.writer(outfile).writerows(repositories)
     elif fetch_type == 'prs':
         prs = gh.get_prs(organization=args.organization, repository=args.repository, progress_cb=progress_cb)
-        for pr in prs:
-            print_pr(pr=pr, long_format=args.long)
+        if args.format == OutputFormat.SHORT or args.format == OutputFormat.LONG:
+            for pr in prs:
+                print_pr(pr=pr, format=args.format, outfile=outfile)
+        elif args.format == OutputFormat.JSON:
+            json.dump(prs, outfile)
+        elif args.format == OutputFormat.CSV:
+            csv.writer(outfile).writerows(prs)
     else:
         print("Invalid fetch type")
         exit(1)
+
+    if args.outfile:
+        outfile.close()
     exit(0)
     
--- src/vegomatic/__init__.py
+++ src/vegomatic/__init__.py
@@ -1,9 +1,11 @@
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 # Import main modules
 from .gqlfetch import GqlFetch, PageInfo
+from .gqlf_github import GqlFetchGithub
 
 __all__ = [
     "GqlFetch",
+    "GqlFetchGithub",
     "PageInfo",
 ]
